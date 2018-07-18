a282ad62f4437f255c6957a30215c141f1af3f80
Geo Perez <geovanni.perez@gmail.com>
2018-07-18 09:14:10 -0500

    Adding initial models

---
+++ src/Models/index.ts
@@ -0,0 +1,7 @@
+import { AggregateFunctions, ColumnDataType, ColumnSortDirection, CompareOperators } from './Column';
+import ColumnModel from './ColumnModel';
+
+export {
+    AggregateFunctions, ColumnDataType, ColumnModel, ColumnSortDirection,
+    CompareOperators
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {
+    AggregateFunctions, ColumnDataType, ColumnModel, ColumnSortDirection,
+    CompareOperators
+} from './Models';
+
+export {
+    AggregateFunctions, ColumnDataType, ColumnModel, ColumnSortDirection,
+    CompareOperators
+};
