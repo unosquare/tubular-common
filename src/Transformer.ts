e05d1a1ce3260a517422280f7372dd1a15507e33
Geo Perez <geovanni.perez@gmail.com>
2020-03-25 10:09:05 -0600

    Lint

--- src/Transformer.ts
+++ src/Transformer.ts
@@ -52,7 +52,7 @@ export class Transformer {
                     return subset;
                 }
 
-                return subset.filter(item =>
+                return subset.filter((item) =>
                     searchableColumns.some((x: ColumnModel) => {
                         if (typeof item[x.name] === 'undefined') {
                             return false;
@@ -86,14 +86,14 @@ export class Transformer {
                 switch (column.filter.operator) {
                     case CompareOperators.Equals:
                         if (isDate) {
-                            subset = subset.filter(row => isEqual(row[column.name], column.filter.text));
+                            subset = subset.filter((row) => isEqual(row[column.name], column.filter.text));
                         } else if (column.dataType === ColumnDataType.String) {
                             subset = partialfiltering(
                                 subset,
                                 (x: string) => x.toLowerCase() === column.filter.text.toLowerCase(),
                             );
                         } else {
-                            subset = subset.filter(row => row[column.name] === column.filter.text);
+                            subset = subset.filter((row) => row[column.name] === column.filter.text);
                         }
                         break;
                     case CompareOperators.NotEquals:
@@ -103,13 +103,13 @@ export class Transformer {
                                 (x: string) => x.toLowerCase() !== column.filter.text.toLowerCase(),
                             );
                         } else {
-                            subset = subset.filter(row => row[column.name] !== column.filter.text);
+                            subset = subset.filter((row) => row[column.name] !== column.filter.text);
                         }
                         break;
                     case CompareOperators.Contains:
                         subset = partialfiltering(
                             subset,
-                            x => x.toLowerCase().indexOf(column.filter.text.toLowerCase()) >= 0,
+                            (x) => x.toLowerCase().indexOf(column.filter.text.toLowerCase()) >= 0,
                         );
                         subset = partialfiltering(
                             subset,
@@ -145,44 +145,44 @@ export class Transformer {
                         break;
                     case CompareOperators.Gt:
                         if (isDate) {
-                            subset = subset.filter(row => isAfter(row[column.name], column.filter.text));
+                            subset = subset.filter((row) => isAfter(row[column.name], column.filter.text));
                         } else {
-                            subset = subset.filter(row => row[column.name] > column.filter.text);
+                            subset = subset.filter((row) => row[column.name] > column.filter.text);
                         }
                         break;
                     case CompareOperators.Gte:
                         if (isDate) {
                             subset = subset.filter(
-                                row =>
+                                (row) =>
                                     isEqual(row[column.name], column.filter.text) ||
                                     isAfter(row[column.name], column.filter.text),
                             );
                         } else {
-                            subset = subset.filter(row => row[column.name] >= column.filter.text);
+                            subset = subset.filter((row) => row[column.name] >= column.filter.text);
                         }
                         break;
                     case CompareOperators.Lt:
                         if (isDate) {
-                            subset = subset.filter(row => isBefore(row[column.name], column.filter.text));
+                            subset = subset.filter((row) => isBefore(row[column.name], column.filter.text));
                         } else {
-                            subset = subset.filter(row => row[column.name] < column.filter.text);
+                            subset = subset.filter((row) => row[column.name] < column.filter.text);
                         }
                         break;
                     case CompareOperators.Lte:
                         if (isDate) {
                             subset = subset.filter(
-                                row =>
+                                (row) =>
                                     isEqual(row[column.name], column.filter.text) ||
                                     isBefore(row[column.name], column.filter.text),
                             );
                         } else {
-                            subset = subset.filter(row => row[column.name] <= column.filter.text);
+                            subset = subset.filter((row) => row[column.name] <= column.filter.text);
                         }
                         break;
                     case CompareOperators.Between:
                         if (isDate) {
                             subset = subset.filter(
-                                row =>
+                                (row) =>
                                     (isEqual(row[column.name], column.filter.text) ||
                                         isAfter(row[column.name], column.filter.text)) &&
                                     (isEqual(row[column.name], column.filter.argument[0]) ||
@@ -190,7 +190,7 @@ export class Transformer {
                             );
                         } else {
                             subset = subset.filter(
-                                row =>
+                                (row) =>
                                     row[column.name] >= column.filter.text &&
                                     row[column.name] <= column.filter.argument[0],
                             );
