64b8da7d9a3b2e68b1c0e63ca48d3a026379a5f6
Jeff Hage <ownerejeff@gmail.com>
2020-03-31 14:17:40 -0400

    Update unit test

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -3,13 +3,18 @@ import { ColumnDataType, createColumn, getCsv } from '../src';
 describe('getCsv', () => {
     it('should not return undefined if a column is not visible', () => {
         const columns = [
-            createColumn('test', {
-                label: 'test column',
+            createColumn('first', {
+                label: 'first column',
                 visible: true,
                 dataType: ColumnDataType.String,
             }),
-            createColumn('broken', {
-                label: 'broken column',
+            createColumn('second', {
+                label: 'second column',
+                visible: true,
+                dataType: ColumnDataType.String,
+            }),
+            createColumn('hidden', {
+                label: 'hidden column',
                 visible: false,
                 dataType: ColumnDataType.String,
             }),
@@ -17,17 +22,26 @@ describe('getCsv', () => {
 
         const data = [
             {
-                test: 'test value!',
-                broken: 'broken value!',
+                first: 'first value 1!',
+                second: 'second value 1!',
+                hidden: 'hidden value 1!',
             },
             {
-                test: 'test value 2!',
-                broken: 'broken value 2!',
+                first: 'first value 2!',
+                second: 'second value 2!',
+                hidden: 'hidden value 2!',
+            },
+            {
+                first: 'first value 3!',
+                second: 'second value 3!',
+                hidden: 'hidden value 3!',
             },
         ] as any;
 
         const output = getCsv(data, columns);
 
-        expect(output).toEqual('test column\ntest value!\ntest value 2!\n');
+        expect(output).toEqual(
+            'first column,second column\nfirst value 1!,second value 1!\nfirst value 2!,second value 2!\nfirst value 3!,second value 3!\n',
+        );
     });
 });
