e05136726bf873f240cafedc3d9bc970e26fa80b
Erick Garcia <erick.garcia@unosquare.com>
2020-05-19 13:17:30 -0500

    updates

--- test/mock/index.ts
+++ test/mock/index.ts
@@ -16,4 +16,3 @@ export const mockColumnModel: ColumnModel = {
     sortable: false,
     visible: false
 };
-
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,4 +1,4 @@
-import { ColumnDataType, createColumn, getCsv, formatDate, getColumnAlign, parsePayload } from '../src';
+import { ColumnDataType, createColumn, getCsv, formatDate, getColumnAlign, parsePayload, getHtml } from '../src';
 import { mockColumnModel } from './mock';
 
 //started
@@ -72,6 +72,50 @@ describe('getCsv', () => {
 
         expect(output).toEqual(
             'first column,second column\nfirst value 1!,second value 1!\nfirst value 2!,second value 2!\nfirst value 3!,second value 3!\n',
+        );
+    });
+
+    it('getHtml', () => {
+        const columns = [
+            createColumn('first', {
+                label: 'first column',
+                visible: true,
+                dataType: ColumnDataType.String,
+            }),
+            createColumn('second', {
+                label: 'second column',
+                visible: true,
+                dataType: ColumnDataType.String,
+            }),
+            createColumn('hidden', {
+                label: 'hidden column',
+                visible: false,
+                dataType: ColumnDataType.String,
+            }),
+        ];
+
+        const data = [
+            {
+                first: 'first value 1!',
+                second: 'second value 1!',
+                hidden: 'hidden value 1!',
+            },
+            {
+                first: 'first value 2!',
+                second: 'second value 2!',
+                hidden: 'hidden value 2!',
+            },
+            {
+                first: 'first value 3!',
+                second: 'second value 3!',
+                hidden: 'hidden value 3!',
+            },
+        ] as any;
+
+        const output = getHtml(data, columns);
+
+        expect(output).toEqual(
+            '<table class=\"table table-bordered table-striped\"><thead><tr><th>first column</th><th>second column</th></tr></thead><tbody><tr><td>first value 1!</td><td>second value 1!</td></tr><tr><td>first value 2!</td><td>second value 2!</td></tr><tr><td>first value 3!</td><td>second value 3!</td></tr></tbody></table>',
         );
     });
 });
