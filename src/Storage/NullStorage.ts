d916b26e2dc076349ed93fcadb623b6dadfc0920
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2024-11-15 08:59:29 -0600

    [skip ci] Remove prettier

--- src/Storage/NullStorage.ts
+++ src/Storage/NullStorage.ts
@@ -2,17 +2,14 @@ import type { ColumnModel } from '../Models';
 import type DataGridStorage from './DataGridStorage';
 
 class NullStorage implements DataGridStorage {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public setTextSearch(_textSearch: string): void {
         // do nothing
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public setPage(_page: number): void {
         // do nothing
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public setColumns(_columns: ColumnModel[]): void {
         // do nothing
     }
